e4a03ed43c133723f965d6c0aa1e8829b6ffd5eb
saltire sable <saltiresable@gmail.com>
2013-01-15 22:17:49 -0500

    Menu options now render in multiple columns
    Columns now also scroll horizontally if there are too many to fit

--- roverchip/menu.py
+++ roverchip/menu.py
@@ -5,23 +5,28 @@ from screen import Screen
 
 
 class Menu(Screen):
+    arrow_keys = pygame.K_UP, pygame.K_RIGHT, pygame.K_DOWN, pygame.K_LEFT
+
     def __init__(self, window, levels):
-        # init subwindow
+        # subwindow properties
         self.basesize = 3, 2
-        self.maxsize = .9, .9
+        self.margin = 0.05, 0.05
+        self.textmargin = 0.05, 0.1
+        
+        # text properties
+        self.maxrows = 8
+        self.maxsize = 100
+        self.minsize = 30
+        self.cols = 2
         
         # init menu options
-        self.fontheight = 50
+        self.menu = 'main'
         self.mainmenu = [('play', 'Play Game'),
                         ('quit', 'Quit Game'),
                         ]
         self.options = self.mainmenu
-
-        # init marker
         self.selected = 0
-        self.marker = pygame.surface.Surface((20, 20))
-        self.marker.fill((255, 0, 0))
-        self.arrow_timeout = 0
+        self.col_offset = 0
         
         # init levels
         self.levels = levels
@@ -35,8 +40,8 @@ class Menu(Screen):
         # and a maximum of maxsize on either axis.
         ww, wh = size
         bw, bh = self.basesize
-        mw, mh = self.maxsize
-        mult = min(ww * mw / bw, wh * mh / bh)
+        mx, my = self.margin
+        mult = min(ww * (1 - mx * 2) / bw, wh * (1 - my * 2) / bh)
         width, height = bw * mult, bh * mult
         left, top = (ww - width) / 2, (wh - height) / 2
         self.view = self.window.view.subsurface((left, top, width, height))
@@ -45,67 +50,121 @@ class Menu(Screen):
         self.background = pygame.Surface(self.view.get_size())
         self.background.fill((255, 255, 255))
         
+        # create text area
+        tmx, tmy = self.textmargin
+        tleft, ttop = tmx * width, tmy * height
+        twidth, theight = width - tleft * 2, height - ttop * 2
+        self.textarea = self.view.subsurface((tleft, ttop, twidth, theight))
+        
+        # find biggest font size that will fit the max number of rows
+        # but without going under the min size
+        self.rheight = next(size for size in range(self.maxsize, self.minsize - 1, -1)
+                        if size * self.maxrows < self.textarea.get_height()
+                        or size == self.minsize)
+        self.font = pygame.font.Font(None, self.rheight)
+        
+        # reduce the number of rows if font is at min size
+        # and max rows still don't fit
+        self.rows = next(num for num in range(self.maxrows, 0, -1)
+                         if num * self.rheight < self.textarea.get_height()
+                         or num == 1)
+
+        # draw marker
+        msize = self.font.get_height() / 2
+        self.marker = pygame.Surface((msize, msize))
+        self.marker.fill((255, 0, 0))
+
         self.redraw = True
 
 
     def run_frame(self, elapsed, keys):
-        if self.arrow_timeout > 0:
-            self.arrow_timeout -= elapsed
-        
         for key, keydown in keys:
-            # move marker up or down
-            if (keydown and key in (pygame.K_UP, pygame.K_DOWN)
-                and self.arrow_timeout <= 0):
-                mod = 1 if key == pygame.K_DOWN else -1
-                self.selected = (self.selected + mod) % len(self.options)
-                self.arrow_timeout = 100
-                self.redraw = True
-                
-            # return selection on hitting enter
+            # arrow keys: change selection
+            if keydown and key in self.arrow_keys:
+                col = self.selected / self.rows
+                totalcols = (len(self.options) + self.rows - 1) / self.rows
+                old_selected = self.selected
+
+                if key in (pygame.K_UP, pygame.K_DOWN):
+                    # move marker up or down
+                    mod = 1 if key == pygame.K_DOWN else -1
+                    self.selected = max(0, min(self.selected + mod, len(self.options) - 1))
+                    
+                elif key == pygame.K_LEFT and col > 0:
+                    # move marker left
+                    self.selected -= self.rows
+                    
+                elif key == pygame.K_RIGHT and col < totalcols - 1:
+                    # move marker right
+                    self.selected = min(self.selected + self.rows,
+                                        len(self.options) - 1)
+                    
+                if self.selected != old_selected:
+                    self.redraw = True
+                    
+            # enter key: act on selection value
             elif keydown and key == pygame.K_RETURN:
-                action = self.options[self.selected][0]
+                value = self.options[self.selected][0]
                 
-                if action == 'play':
-                    self.options = ([(i, 'Level ' + str(i + 1))
-                                     for i in range(len(self.levels))]
-                                    + [('back', 'Back')])
-                    self.selected = 0
-                    self.redraw = True
-                    
-                elif action == 'quit':
-                    return 'quit'
+                if self.menu == 'main':
+                    if value == 'play':
+                        self.menu = 'levels'
+                        self.options = [(i, 'Level ' + str(i + 1))
+                                        for i in range(30)]
+                                        #for i in range(len(self.levels))]
+                        self.selected = 0
+                        self.redraw = True
+                        
+                    elif value == 'quit':
+                        return 'quit'
                 
-                elif action == 'back':
-                    self.options = self.mainmenu
-                    self.selected = 0
-                    self.redraw = True
-                    
-                else:
+                elif self.menu == 'levels':
                     # start the game screen, skipping to the selected level
-                    self.window.run(Game(self.window, self.levels, action))
+                    self.window.run(Game(self.window, self.levels, value))
+                    self.menu = 'main'
                     self.options = self.mainmenu
                     self.selected = 0
                     self.redraw = True
             
-    
+            # escape key: quit menu
+            elif keydown and key == pygame.K_ESCAPE:
+                if self.menu == 'main':
+                    print 'quitting'
+                    return 'quit'
+                
+                elif self.menu == 'levels':
+                    print 'yikes'
+                    self.menu = 'main'
+                    self.options = self.mainmenu
+                    self.selected = 0
+                    self.redraw = True
+                
+
     def draw_frame(self):
         if self.redraw:
             # blit the background, text and marker onto the view
             self.view.blit(self.background, (0, 0))
             
-            maxsize = 50
-            minsize = 20
-            fontsize = next(size for size in range(maxsize, minsize - 1, -1)
-                            if size * len(self.options) < self.view.get_height()
-                            or size == minsize)
+            fheight = self.font.get_height()
+            colwidth = self.textarea.get_width() / self.cols
             
-            left, top = 50, 0
-            font = pygame.font.Font(None, fontsize)
-            for i, (_, text) in enumerate(self.options):
-                self.view.blit(font.render(text, True, (0, 0, 0)),
-                                     (left, top + fontsize * i))
-            left, top = 15, 5
-            self.view.blit(self.marker, (left, top + fontsize * self.selected))
+            row = self.selected % self.rows
+            col = self.selected / self.rows
+            # adjust offset to within (self.cols) of col
+            self.col_offset = min(col, max(self.col_offset, col - self.cols + 1))
+            options = self.options[self.col_offset * self.rows:
+                                   (self.col_offset + self.cols) * self.rows]
+            
+            for i, (_, text) in enumerate(options):
+                # blit text onto its row, indenting by fheight
+                self.textarea.blit(self.font.render(text, True, (0, 0, 0)),
+                                   (i / self.rows * colwidth + fheight,
+                                    i % self.rows * self.rheight))
+            
+            # blit marker
+            mmargin = self.font.get_height() / 4
+            self.textarea.blit(self.marker, ((col - self.col_offset) * colwidth + mmargin,
+                                             row * self.rheight + mmargin))
             
             self.redraw = False
 
