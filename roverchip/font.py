ff897d3484e8bbbc04ae4971c919a8c787a6cf2b
saltire sable <saltiresable@gmail.com>
2013-01-16 20:36:43 -0500

    Added sizing and colour to the font class
    Now you can pass a line size to the font render method, and it will
    resize the characters.
    
    You can also pass a colour.
    
    Also, when initializing the font, you can specify monospace, or failing
    that a width for the space character.

--- roverchip/font.py
+++ roverchip/font.py
@@ -2,43 +2,66 @@ import pygame
 
 
 class Font:
-    def __init__(self, spritepath='font.png', charsize=(8, 8)):
+    def __init__(self, spritepath, charsize, mono=False, spacewidth=None):
         """Given a sprite that is a grid of characters, and the dimensions
         of each character, slice it into a dict of surfaces mapped to each
         character in ASCII sequence, starting at 0."""
         self.sprite = pygame.image.load(spritepath)
         cw, ch = charsize
         sw, sh = self.sprite.get_width() / cw, self.sprite.get_height() / ch
+        spacewidth = spacewidth if spacewidth is not None else cw * .4
         
         self.height = ch
-        
+
         self.chars = {}
         for y in range(sh):
             for x in range(sw):
-                char = self.sprite.subsurface((x * cw, y * ch, cw, ch))
-                cwidth = char.get_bounding_rect()[2]
-                self.chars[chr(x + y * sw)] = char.subsurface((0, 0, cwidth, ch))
+                char = chr(x + y * sw)
+                charimg = self.sprite.subsurface((x * cw, y * ch, cw, ch))
+                
+                # crop characters to their actual width, unless monospaced
+                if not mono:
+                    cwidth = (charimg.get_bounding_rect()[2]
+                              if char != ' ' else spacewidth)
+                    charimg = charimg.subsurface((0, 0, cwidth, ch))
+                    
+                self.chars[char] = charimg
     
     
-    def render_text(self, text, scale=1, kerning=1, leading=1):
-        """Return a surface containing a graphical rendering of the text."""
+    def render(self, text, lineheight=None, leading=0, kerning=0, color=None):
+        """Return a surface containing a graphical rendering of the text.
+        If lineheight is passed, scale the characters to that height.
+        Color will replace all colours, keeping alpha unchanged."""
+        chars = {char: self.chars[char] for char in
+                 set(char for char in text.replace('\n', ''))}
+        
+        if color is not None:
+            for charimg in chars.itervalues():
+                # replace RGB channels with those from color
+                pix = pygame.surfarray.pixels3d(charimg)
+                pix[:,:,0], pix[:,:,1], pix[:,:,2] = color
+                del(pix)
+            
+        if lineheight is None:
+            lineheight = self.height
+        else:
+            # scale up each character to the line height
+            scale = float(lineheight) / self.height
+            for char, charimg in chars.iteritems():
+                charwidth = int(charimg.get_width() * scale)
+                chars[char] = pygame.transform.scale(charimg, (charwidth, lineheight))
+                            
         text = text.split('\n')
-        width = max(sum(self.chars[char].get_width() + kerning for char in line)
+        width = max(sum(chars[char].get_width() + kerning for char in line)
                     - kerning
                     for line in text)
-        height = self.get_height(len(text), leading)
-        canvas = pygame.Surface((width, height))
+        height = (lineheight + leading) * len(text) - leading
+        canvas = pygame.Surface((width, height), pygame.SRCALPHA)
         
         for y, line in enumerate(text):
             x = 0
             for char in line:
-                canvas.blit(self.chars[char], (x, y * (self.height + leading)))
-                x += self.chars[char].get_width() + kerning
+                canvas.blit(chars[char], (x, y * (lineheight + leading)))
+                x += chars[char].get_width() + kerning
                 
-        return (canvas if scale == 1 else
-                pygame.transform.scale(canvas, (width * scale, height * scale)))
-
-    
-    def get_height(self, lines=1, leading=0):
-        """Return the height of the given number of lines at the given leading."""
-        return (self.height + leading) * lines
+        return canvas
